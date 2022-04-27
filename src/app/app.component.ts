7f830f35d600c697335699bf8d1b354bb3466381
nelsoneo <nelsoneo@gmail.com>
2022-04-26 22:55:58 -0300

    directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-course-directive';
+export class AppComponent implements OnInit {
+ 
+  count = 0;
+
+  constructor(){}
+
+  ngOnInit() {
+
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10){
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
 }
