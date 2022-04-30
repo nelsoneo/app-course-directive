b66cb0db3821988c647f94f4176d1a0bf5392b55
nelsoneo <nelsoneo@gmail.com>
2022-04-29 23:45:39 -0300

    service more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { MarcaTextDirective } from '../app/shared/directives/marca-text.directive';
 import { PeopleService } from './shared/services/people.service';
+import { ListApiComponent } from './shared/components/list-api/list-api.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarcaTextDirective
+    MarcaTextDirective,
+    ListApiComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [PeopleService],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/list-api/list-api.component.ts
@@ -0,0 +1,26 @@
+
+import { Component, OnInit } from '@angular/core';
+import { ListService } from '../../services/list.service';
+
+@Component({
+  selector: 'app-list-api',
+  templateUrl: './list-api.component.html',
+  styleUrls: ['./list-api.component.scss']
+})
+export class ListApiComponent implements OnInit {
+  personagens: Array<any> = [];
+
+  constructor(private listService: ListService) { }
+
+  ngOnInit(): void {
+    this.getList();
+  }
+
+  getList() {
+    this.listService.getList().subscribe(result => {
+      this.personagens = result?.results;
+      console.log(this.personagens);
+    })
+  }
+
+}
